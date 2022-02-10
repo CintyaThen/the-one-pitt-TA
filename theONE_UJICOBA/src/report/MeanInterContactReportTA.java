14bc10026928cb26e5f4ced18d9b7f8438d79ea2
Cintya Then <65504591+CintyaThen@users.noreply.github.com>
2022-02-10 17:08:01 +0700

    Update MeanInterContactReportTA.java

--- theONE_UJICOBA/src/report/MeanInterContactReportTA.java
+++ theONE_UJICOBA/src/report/MeanInterContactReportTA.java
@@ -26,52 +26,50 @@ public class MeanInterContactReportTA extends Report{
 	
 	@Override
     public void done() {
-		String reportText="";
-        List<DTNHost> nodes = SimScenario.getInstance().getHosts();
-        
-        for (DTNHost h : nodes) {
-        	MessageRouter r = h.getRouter();
-        	DecisionEngineRouter de = (DecisionEngineRouter) r;
-        	BubbleRap br = (BubbleRap) de.getDecisionEngine();
-        	InterconnectDE
-        	
-            if (!(r instanceof DecisionEngineRouter)) {
-                continue;
-            }
-            RoutingDecisionEngine de = ((DecisionEngineRouter) r).getDecisionEngine();
-            if (!(de instanceof ConnListDecisionEngine)) {
-                continue;
-            }
-            ConnListDecisionEngine cld = (ConnListDecisionEngine) de;
+		   List<DTNHost> nodes = SimScenario.getInstance().getHosts();
+	        String print="";
+	        for (DTNHost h : nodes) {
+	        	print += "\n"+h+""; 
+	            MessageRouter r = h.getRouter();
+	            if (!(r instanceof DecisionEngineRouter)) {
+	                continue;
+	            }
+	            RoutingDecisionEngine de = ((DecisionEngineRouter) r).getDecisionEngine();
+	            if (!(de instanceof ConnListDecisionEngine)) {
+	                continue;
+	            }
+	            ConnListDecisionEngine cld = (ConnListDecisionEngine) de;
 
-            Map<DTNHost, List<Duration>> nodeConnList = cld.getConnList(); //Mengambil lokal komunitas yang dimiliki host tersebut
-            
-            for (Map.Entry<DTNHost, List<Duration>> entry : nodeConnList.entrySet()) {
+	            Map<DTNHost, List<Duration>> nodeConnList = cld.getConnList(); //Mengambil lokal komunitas yang dimiliki host tersebut
+	            
+	            for (Map.Entry<DTNHost, List<Duration>> entry : nodeConnList.entrySet()) {
 
-            	List<Duration> nodeDuration = entry.getValue();
-            	Iterator<Duration> i = nodeDuration.iterator();
-            	double endTimebefore=0;
-            	boolean first = true;
-            	List<Double> durationList = new LinkedList();
-            	while(i.hasNext()) {
-            		Duration d = i.next();
-            		if(first==false) {
-            			durationList.add(d.start-endTimebefore);
-            		}	
-            		else {
-            			first=false;
-            		}
-            		endTimebefore=d.end;
-            	}
-            	if(durationList.size()==0) {
-            		continue;
-            	}
-            	print+=", "+ getAverage(durationList);
-            	
-            }
+	            	List<Duration> nodeDuration = entry.getValue();
+	            	Iterator<Duration> i = nodeDuration.iterator();
+	            	double endTimebefore=0;
+	            	boolean first = true;
+	            	List<Double> durationList = new LinkedList();
+	            	
+	            	while(i.hasNext()) {
+	            		Duration d = i.next();
+	            		if(first==false) {
+	            			durationList.add(d.start-endTimebefore);
+	            		}	
+	            		else {
+	            			first=false;
+	            		}
+	            		endTimebefore=d.end;
+	            	}
+	            	if(durationList.size()==0) {
+	            		continue;
+	            	}
+	            	
+	            	print+=" "+ getVariance(durationList);
+	            	
+	            }
 
-        }
-        write(print);
-        super.done();
+	        }
+	        write(print);
+	        super.done();
 	}
 }

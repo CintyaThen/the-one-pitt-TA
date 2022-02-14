5af5a7da4abacad4779131654b19b6689127f1f5
Cintya Then <65504591+CintyaThen@users.noreply.github.com>
2022-02-14 15:52:49 +0700

    Coba bongkar durasi

--- theONE_UJICOBA/src/report/ClosenessNodeTiapWaktuReport.java
+++ theONE_UJICOBA/src/report/ClosenessNodeTiapWaktuReport.java
@@ -7,6 +7,7 @@ package report;
 import java.util.*;
 //import java.util.List;
 //import java.util.Map;
+import java.util.Map.Entry;
 
 import core.DTNHost;
 import core.Message;
@@ -33,44 +34,35 @@ public class ClosenessNodeTiapWaktuReport extends Report {
     @Override
     public void done() {
         List<DTNHost> nodes = SimScenario.getInstance().getHosts();
-        for (DTNHost ho : nodes) {
-            MessageRouter r = ho.getRouter();
-            if (!(r instanceof DecisionEngineRouter)) {
-                continue;
-            }
-            RoutingDecisionEngine de = ((DecisionEngineRouter) r).getDecisionEngine();
-            if (!(de instanceof ClosenessDecisionEngine)) {
-                continue;
-            }
-            ClosenessDecisionEngine cd = (ClosenessDecisionEngine) de;
-            Map<DTNHost, List<Double>> closenessCounter = cd.getCloseness();
-            
-            for (Map.Entry<DTNHost, List<Double>> entry : closenessCounter.entrySet()) {
-                String print = "";
-                for (Double value : entry.getValue()) {
-                    print = print + "\n" + value;
-                }
-                write(print);
+		/*
+		 * for (DTNHost ho : nodes) { MessageRouter r = ho.getRouter(); if (!(r
+		 * instanceof DecisionEngineRouter)) { continue; } RoutingDecisionEngine de =
+		 * ((DecisionEngineRouter) r).getDecisionEngine(); if (!(de instanceof
+		 * ClosenessDecisionEngine)) { continue; } ClosenessDecisionEngine cd =
+		 * (ClosenessDecisionEngine) de; Map<DTNHost, List<Double>> closenessCounter =
+		 * cd.getCloseness();
+		 * 
+		 * for (Map.Entry<DTNHost, List<Double>> entry : closenessCounter.entrySet()) {
+		 * String print = ""; for (Double value : entry.getValue()) { print = print +
+		 * "\n" + value; } write(print); } }
+		 */
+
+        for (DTNHost node : nodes) {
+
+            if (closenessCount.containsKey(node)) {
+                double avgCloseness = getAverageCloseness(closenessCount.get(node));
+             //   encounterData.put(node, nodes.ge );
             }
         }
-
-//        for (DTNHost node : nodes) {
-
-
-//            if (closenessCount.containsKey(node)) {
-//                double avgCloseness = getAverageCloseness(closenessCount.get(node));
-//                encounterData.put(node, avgCloseness);
-//            }
-//        }
-//        System.out.println(closenessCount);
-//        String print = "";
-//        for (Map.Entry<DTNHost, List<Double>> entry : encounterData.entrySet()) {
-//            DTNHost key = entry.getKey();
-//            for (Double value : entry.getValue()) {
-//                print = print + "\n" + value;
-//            }
-//        }
-//        write(print);
+        System.out.println(closenessCount);
+        String print = "";
+        for (Map.Entry<DTNHost, List<Double>> entry : encounterData.entrySet()) {
+            DTNHost key = entry.getKey();
+            for (Double value : entry.getValue()) {
+                print = print + "\n" + value;
+            }
+        }
+        write(print);
         super.done();
     }
 
--- theONE_UJICOBA/src/report/MeanInterContactReportTA.java
+++ theONE_UJICOBA/src/report/MeanInterContactReportTA.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 import core.DTNHost;
+import core.SimClock;
 import core.SimScenario;
 import routing.DecisionEngineRouter;
 import routing.MessageRouter;
@@ -15,6 +16,7 @@ import routing.RoutingDecisionEngine;
 import routing.community.BubbleRap;
 import routing.community.CommunityDetectionEngine;
 import routing.community.ConnListDecisionEngine;
+import routing.community.DeliveryPredictabilityDecisionEngine;
 import routing.community.DistributedBubbleRap;
 import routing.community.Duration;
 
@@ -26,50 +28,71 @@ public class MeanInterContactReportTA extends Report{
 	
 	@Override
     public void done() {
+		 int currentHourCount;
+		 int currentHour;
+		 
+		 
 		   List<DTNHost> nodes = SimScenario.getInstance().getHosts();
-	        String print="";
+	        String println="";
 	        for (DTNHost h : nodes) {
-	        	print += "\n"+h+""; 
-	            MessageRouter r = h.getRouter();
-	            if (!(r instanceof DecisionEngineRouter)) {
-	                continue;
-	            }
-	            RoutingDecisionEngine de = ((DecisionEngineRouter) r).getDecisionEngine();
-	            if (!(de instanceof ConnListDecisionEngine)) {
-	                continue;
-	            }
-	            ConnListDecisionEngine cld = (ConnListDecisionEngine) de;
+	        	println += "\n" + h +" ";
+	        	MessageRouter r = h.getRouter();
+	        	if(!(r instanceof DecisionEngineRouter) )
+	    			continue;
+	    		RoutingDecisionEngine de = ((DecisionEngineRouter)r).getDecisionEngine();
+	    		if(!(de instanceof ConnListDecisionEngine))
+	    			continue;
+	    		ConnListDecisionEngine dp = (ConnListDecisionEngine)de;
+	    		 
+	    		Map <DTNHost, List<Duration>> contactList = dp.getConnList();//Bongkar disini
+	    		
+	    		
+	    		
+	    		for (Map.Entry<DTNHost, List<Duration>> entry : contactList.entrySet()) {
+					  
+					List<Duration> nodeDuration = entry.getValue(); //Bongkar disini
+					
+					Iterator<Duration> i = nodeDuration.iterator() ; //Bongkar disini
+					double endTimebefore = 0;
+					boolean first = true;
+					
+					
+					List<Double> durationList = new LinkedList();
+					while (i.hasNext() ) {
+						Duration d = i.next();
+						if (first == false) {
+							durationList.add(d.start - endTimebefore);
+						} else {
+							first = false;
+						}
+						endTimebefore = d.end;
+					}
+					if (durationList.size() == 0) {
+						continue;
+					} // Map
 
-	            Map<DTNHost, List<Duration>> nodeConnList = cld.getConnList(); //Mengambil lokal komunitas yang dimiliki host tersebut
-	            
-	            for (Map.Entry<DTNHost, List<Duration>> entry : nodeConnList.entrySet()) {
-
-	            	List<Duration> nodeDuration = entry.getValue();
-	            	Iterator<Duration> i = nodeDuration.iterator();
-	            	double endTimebefore=0;
-	            	boolean first = true;
-	            	List<Double> durationList = new LinkedList();
-	            	
-	            	while(i.hasNext()) {
-	            		Duration d = i.next();
-	            		if(first==false) {
-	            			durationList.add(d.start-endTimebefore);
-	            		}	
-	            		else {
-	            			first=false;
-	            		}
-	            		endTimebefore=d.end;
-	            	}
-	            	if(durationList.size()==0) {
-	            		continue;
-	            	}
-	            	
-	            	print+=" "+ getVariance(durationList);
-	            	
+					println += " " + getAverage(durationList);
+				 
 	            }
 
 	        }
-	        write(print);
+	        write(println);
 	        super.done();
+	}
+	private double getAverageInterContact (List<Double> durationList) {
+		Iterator<Double> i = durationList.iterator();
+		while (i.hasNext() ) {
+			//Duration d = i.next();
+			if (first == false) {
+				durationList.add(d.start - endTimebefore);
+			} else {
+				first = false;
+			}
+			endTimebefore = d.end;
+		}
+		if (durationList.size() == 0) {
+			continue;
+		}
+		return getAverage(durationList);
 	}
 }
